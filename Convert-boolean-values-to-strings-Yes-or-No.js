c4032e77fae77336e44214d0868d1745adf8b692
Julyetta Karol <julietta_karol@mail.ru>
2024-02-05 15:56:59 +0300

    added new task

---
+++ Convert-boolean-values-to-strings-Yes-or-No.js
@@ -0,0 +1,7 @@
+// Complete the method that takes a boolean value and 
+// return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    let str = bool ? "Yes" : "No"
+    return str
+  }
