b387ae06f0434bdadbdc253d1ec138b42dba8e80
Julyetta Karol <julietta_karol@mail.ru>
2024-02-06 15:02:57 +0300

    added new task

---
+++ Opposite-number.js
@@ -0,0 +1,7 @@
+// Very simple, given a number 
+// (integer / decimal / both depending on the language), 
+// find its opposite (additive inverse).
+
+function opposite(number) {
+    return (number < 0) ? -number : -number 
+  }
