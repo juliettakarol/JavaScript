ae82e51494cd9068c20045602c0811945ac77acd
Julyetta Karol <julietta_karol@mail.ru>
2024-02-08 15:44:15 +0300

    added new task

---
+++ Basic-Mathematical-Operations.js
@@ -0,0 +1,16 @@
+// Your task is to create a function that does four 
+// basic mathematical operations.
+
+// The function should take three arguments - 
+// operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after 
+// applying the chosen operation.
+
+function basicOp(operation, value1, value2){
+    let result
+    if (operation === '+') {result = value1 + value2}
+    if (operation === '-') {result = value1 - value2}
+    if (operation === '*') {result = value1 * value2}
+    if (operation === '/') {result = value1 / value2}
+    return result;
+  }
---
+++ Counting-sheep.js
@@ -0,0 +1,18 @@
+// Consider an array/list of sheep where some sheep 
+// may be missing from their place. 
+// We need a function that counts the number of sheep 
+// present in the array (true means present).
+
+
+function countSheeps(sheep) {
+    let summ = 0
+    sheep.forEach(element => {
+        if (element === undefined){
+            continue;
+        }  
+        summ += Number(element)
+    });
+    return summ
+  }
+
+  console.log (countSheeps[undefined, true])
