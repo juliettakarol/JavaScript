019020b991a9c52b614f5127f217e92ec2991b5e
Julyetta Karol <julietta_karol@mail.ru>
2024-02-15 15:38:42 +0300

    added new task

---
+++ Beginner-Reduce-but-Grow.js
@@ -0,0 +1,6 @@
+// Given a non-empty array of integers, return the 
+// result of multiplying the values together in order.
+
+function grow(x){
+    return x.reduce((mult,elem)=> mult*elem, 0)
+    }
