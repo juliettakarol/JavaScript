6991bfca098851cd61512246d88ec8d1a119ac57
Julyetta Karol <julietta_karol@mail.ru>
2024-03-19 23:12:34 +0300

    added new task

---
+++ 7kyu/Colour-Association.js
@@ -0,0 +1,15 @@
+// Colour plays an important role in our lifes. Most of us like this colour better then another. 
+// User experience specialists believe that certain colours have certain psychological meanings for us.
+
+// You are given a 2D array, composed of a colour and its 'common' association 
+// in each array element. The function you will write needs to return the colour as 'key' and association as its 'value'.
+
+function colourAssociation(array){
+    return array.map(elem => {
+        {elem[0]: elem[1]}
+    })
+  }
+
+
+
+ console.log(colourAssociation([["red", "energy"],["yellow", "creativity"]]))
